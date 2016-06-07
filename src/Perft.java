debf3a417e025357c8f84d1ae7708d5d84e1b707
Michael Radaszkiewicz <mrada6@gmail.com>
2016-06-07 16:29:24 -0400

    Created a clean way to run multiple perfts of a single FEN.

--- src/Perft.java
+++ src/Perft.java
@@ -1,96 +1,111 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+
 public class Perft {
-    private Board board;
+	private Board board;
 
-    public Perft(String fen) {
-        board = new Board(fen);
-        }
+	public Perft(String fen) {
+		board = new Board(fen);
+	}
 
-public Perft() {
-        board = new Board();
-        }
+	public Perft() {
+		board = new Board();
+	}
 
-public long perft(int depth) {
-        ArrayList<Integer> moveList = board.generateMovesNeo(true);
-        if (depth == 1)
-        return moveList.size();
-        long nodes = 0;
-        if (!moveList.equals("")) {
-        for (int i = 0; i < moveList.size(); i++) {
-        board.move(moveList.get(i));
-        nodes += diveIn(depth - 1);
-        board.undo();
-        }
-        }
-        return nodes;
-        }
+	public long perft(int depth) {
+		ArrayList<Integer> moveList = board.generateMovesNeo(true);
+		if (depth == 1)
+			return moveList.size();
+		long nodes = 0;
+		if (!moveList.equals("")) {
+			for (int i = 0; i < moveList.size(); i++) {
+				board.move(moveList.get(i));
+				nodes += diveIn(depth - 1);
+				board.undo();
+			}
+		}
+		return nodes;
+	}
 
-public String divide(int depth) {
-        ArrayList<Integer> moveList = board.generateMovesNeo(true);
-        String[] ary1 = new String[moveList.size()];
-        long[] count = new long[ary1.length];
-        String answer = "";
-        int temp = 0;
-        if (!(moveList.size()==0)) {
-        for (int i = 0; i < moveList.size(); i++) {
-        ary1[temp] = board.translate(""+moveList.get(i));
-        board.move(moveList.get(i));
-        count[temp] = diveIn(depth - 1);
-        board.undo();
-        temp++;
-        }
-        int sum = 0;
-        for (int i = 0; i < ary1.length; i++) {
-        answer += ary1[i] + ": " + count[i] + "\n";
-        sum += count[i];
-        }
-        answer += "\n Moves: " + ary1.length + "\n Nodes: " + sum;
-        }
-        return answer;
-        }
+	public void perft(int start, int end) {
+		String pattern = "0,000.000";
+		DecimalFormat decimalFormat = new DecimalFormat(pattern);
+		for (int i = start; i < end; i++) {
+			String line = "Perft(";
+			if (i < 10)
+				line += " " + i + ")=";
+			Timer t = new Timer();
+			t.start();
+			long n = perft(i);
+			t.stop();
+			long ms = t.getLastLap();
+			String nodeCount = "" + n;
+			String timeString = "( " + decimalFormat.format(ms / 1000.0)
+					+ " sec)";
+			String output = line + nodeCount + timeString;
+			int length = output.length();
+			System.out.print(line);
+			for (int x = 0; x < 40 - length; x++) {
+				System.out.print(" ");
+			}
+			System.out.println(nodeCount + " " + timeString);
+		}
+	}
 
-/*private long diveIn(int depth) {
-        String moveList = board.generateMoves(true);
-        if (moveList.equals("") && depth != 0) {
-        return 0;
-        } else if (moveList.equals("") || depth == 1) {
-        return moveList.length() / 5;
-        }
-        long nodes = 0;
-        if (depth == 0) {
-        return 1;
-        }
+	public String divide(int depth) {
+		ArrayList<Integer> moveList = board.generateMovesNeo(true);
+		String[] ary1 = new String[moveList.size()];
+		long[] count = new long[ary1.length];
+		String answer = "";
+		int temp = 0;
+		if (!(moveList.size() == 0)) {
+			for (int i = 0; i < moveList.size(); i++) {
+				ary1[temp] = board.translate("" + moveList.get(i));
+				board.move(moveList.get(i));
+				count[temp] = diveIn(depth - 1);
+				board.undo();
+				temp++;
+			}
+			int sum = 0;
+			for (int i = 0; i < ary1.length; i++) {
+				answer += ary1[i] + ": " + count[i] + "\n";
+				sum += count[i];
+			}
+			answer += "\n Moves: " + ary1.length + "\n Nodes: " + sum;
+		}
+		return answer;
+	}
 
-        if (!moveList.equals("")) {
-        for (int i = 0; i < moveList.length(); i += 5) {
-        board.move(moveList.substring(i, i + 5));
-        nodes += diveIn(depth - 1);
-        board.undo();
-        }
-        }
-        return nodes;
-        }*/
+	/*
+	 * private long diveIn(int depth) { String moveList =
+	 * board.generateMoves(true); if (moveList.equals("") && depth != 0) {
+	 * return 0; } else if (moveList.equals("") || depth == 1) { return
+	 * moveList.length() / 5; } long nodes = 0; if (depth == 0) { return 1; }
+	 * 
+	 * if (!moveList.equals("")) { for (int i = 0; i < moveList.length(); i +=
+	 * 5) { board.move(moveList.substring(i, i + 5)); nodes += diveIn(depth -
+	 * 1); board.undo(); } } return nodes; }
+	 */
 
+	private long diveIn(int depth) {
+		ArrayList<Integer> moveList = board.generateMovesNeo(true);
+		if (moveList.size() == 0 && depth != 0) {
+			return 0;
+		} else if (moveList.size() == 0 || depth == 1) {
+			return moveList.size();
+		}
+		long nodes = 0;
+		if (depth == 0) {
+			return 1;
+		}
 
-        private long diveIn(int depth) {
-                ArrayList<Integer> moveList = board.generateMovesNeo(true);
-                if (moveList.size()==0 && depth != 0) {
-                        return 0;
-                } else if (moveList.size()==0 || depth == 1) {
-                        return moveList.size();
-                }
-                long nodes = 0;
-                if (depth == 0) {
-                        return 1;
-                }
-
-                if (!moveList.equals("")) {
-                        for (int i = 0; i < moveList.size(); i++) {
-                                board.move(moveList.get(i));
-                                nodes += diveIn(depth - 1);
-                                board.undo();
-                        }
-                }
-                return nodes;
-        }
+		if (!moveList.equals("")) {
+			for (int i = 0; i < moveList.size(); i++) {
+				board.move(moveList.get(i));
+				nodes += diveIn(depth - 1);
+				board.undo();
+			}
+		}
+		return nodes;
+	}
 }
--- src/Runner.java
+++ src/Runner.java
@@ -9,16 +9,22 @@ public class Runner {
 		/*
 		 * UCI u = new UCI(); u.uciCommunication();
 		 */
-		/*for (int x = 0; x < 100; x++) {
-			Timer timer = new Timer();
-			timer.start();
-				Perft p = new Perft();
-				System.out.println(p.perft(5));
-				timer.lap();
-
-			System.out.println(timer.displayLap()+"\n");
-		}*/
-		PerftSuiteTwo ps =new PerftSuiteTwo();
+		/*
+		 * for (int x = 0; x < 100; x++) { Timer timer = new Timer();
+		 * timer.start(); Perft p = new Perft(); System.out.println(p.perft(5));
+		 * timer.lap();
+		 * 
+		 * System.out.println(timer.displayLap()+"\n"); }
+		 */
+		// PerftSuiteTwo ps =new PerftSuiteTwo();
+		Board b = new Board(
+				"rnb1kbnr/pp1pp1pp/1qp2p2/8/Q1P5/N7/PP1PPPPP/1RB1KBNR b Kkq - 2 4");
+		System.out.println(b);
+		// e8f7(60530): 767
+		// b.move(60530);
+		System.out.println(b);
+		Perft p = new Perft(b.getFEN());
+		p.perft(1, 7);
 	}
 
 }
